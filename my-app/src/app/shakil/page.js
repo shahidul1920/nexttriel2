f7a57b8fb4f5ec39ea4d19da390dbfab28683aa5
Shahidul Islam <shahidul1920shakil@gmail.com>
2024-10-06 13:34:04 +0600

    mm

---
+++ my-app/src/app/shakil/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shakilpage = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatem perferendis quidem id, error, maiores quae reiciendis aliquam voluptate ducimus eligendi architecto delectus accusamus libero in distinctio ipsa rem amet voluptatibus nesciunt unde? Unde minus nemo voluptatum ipsa ullam nisi. Accusamus, eligendi numquam aliquam sit, minus sapiente, magni enim fugiat vel atque ex voluptatum! Numquam, fuga esse adipisci excepturi ut pariatur perferendis. Possimus est iure dolorum, nobis repellat doloremque impedit voluptatibus nihil, corrupti autem totam, ex cum illum sapiente perspiciatis illo. Cupiditate voluptatum, rerum sint voluptas natus nisi alias illum aliquam facere maxime sit, corporis minima iste aut, distinctio hic.
+    </div>
+  )
+}
+
+export default Shakilpage
